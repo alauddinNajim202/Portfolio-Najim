cf263f0c876ec316b981efa41acc54ddd798dab8
alauddin-najim <alauddinnajim202@gmail.com>
2023-03-24 23:02:30 +0600

    Najim-Portfolio

---
+++ main.js/main.js
@@ -0,0 +1,15 @@
+ (function($){
+    "use strict"
+
+
+    if($('.typed-text-output').length ==1 ){
+        var typed_strings = $('.typed-text').text();
+        var typed = new typed('.typed-text-output',{
+            strings:typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace : false,
+            loop: true
+        });
+    }
+ })(jQuery);
